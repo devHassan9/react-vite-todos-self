79346663092c8655dbef3b8cc26687e1b910e332
Zeeshan Hassan <hassangill2005@gmail.com>
2026-02-19 13:03:16 +0500

    started todos , completed design and started to add functionalities

--- src/Pages/Dashboard/Todos/Add.jsx
+++ src/Pages/Dashboard/Todos/Add.jsx
@@ -1,11 +1,84 @@
-import { Link } from 'react-router-dom'
+import { Button, Form } from "antd"
+import { Input } from "antd"
+import { Typography } from "antd"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useAuth } from "../../../Context/Auth"
+import { DatePicker } from "antd"
+import { Select } from "antd"
+
+const { Title } = Typography
+const { Item } = Form
+const { Option } = Select
+
+const initialState = { title: "", dueDate: "", description: "", priority: "" }
 
 const Add = () => {
-  return (
-    <main>
-        <Link to="/dashboard/todos" className='btn btn-primary'>All Todo's</Link>
-    </main>
-  )
+
+
+    const { dispatch } = useAuth()
+
+    const [state, setState] = useState(initialState)
+    const [isProcessing, setIsProcessing] = useState(false)
+
+    const navigate = useNavigate()
+
+
+    const handleChange = e => setState(s => ({ ...s, [e.target.name]: e.target.value }))
+
+    const handleLogin = () => {
+
+        let { title, dueDate, description, priority } = state
+
+        title = title.trim()
+
+        if (title.length < 3) { return window.toastify("Please enter full title", "error") }
+
+        const todo = {uid: user.uid ,id: window.getRandomId(), title, dueDate, description, priority, status: "active", isCompleted: "false" }
+
+        setIsProcessing(true)
+        const todos = JSON.parse(localStorage.getItem("todos")) || []
+
+        todos.unshift(todo)
+
+        setTimeout(() => {
+            localStorage.setItem("user", JSON.stringify(user))
+            dispatch({ isAuth: true, user })
+            navigate("/dashboard")
+            setIsProcessing(false)
+            window.toastify("Login successful", "success")
+        }, 500);
+
+    }
+
+    return (
+        <main className='auth flex-center'>
+            <div className="container">
+                <div className="card p-3 p-4 mx-auto">
+                    <Title level={1} className="text-center">Add Todo</Title>
+                    <Form layout="vertical">
+                        <Item label='Title' required>
+                            <Input type='text' size="large" placeholder="Enter title" name="title" onChange={handleChange} />
+                        </Item>
+                        <Item label='Due Date'>
+                            <DatePicker placeholder="Enter due date" name="dueDate" className="w-100" onChange={handleChange} />
+                        </Item>
+                        <Item label='Description'>
+                            <Input.TextArea placeholder="Enter description" name="description" onChange={handleChange} style={{ height: 100, resize: "none" }} />
+                        </Item>
+                        <Item label='Priority'>
+                            <Select size="large" placeholder="Select Priority" >
+                                <Option value="low" >Low</Option>
+                                <Option value="medium" >Medium</Option>
+                                <Option value="high" >High</Option>
+                            </Select>
+                        </Item>
+                        <Button type="primary" size="large" block htmlType="submit" loading={isProcessing} onClick={handleLogin} >Add Todo</Button>
+                    </Form>
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default Add
